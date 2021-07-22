c4d9c7ac9945c73b318ee3bed01d01945a81af91
David Cole <dxcrcc@rit.edu>
2021-07-21 23:24:41 -0400

    Init push

---
+++ src/main/java/com/Blend.java
@@ -0,0 +1,220 @@
+package com;
+
+import java.util.Scanner;
+
+public class Blend {
+
+    private Blend(){}
+
+    protected static Scanner scanner = new Scanner(System.in);
+
+    public static String blendTwo(String hexOne, String hexTwo, String input){
+
+        //Output will be appended over time
+        StringBuilder output = new StringBuilder();
+
+        //Gets RGB values in integer form
+        float hexOneRVal = Integer.parseInt(hexOne.substring(0, 2),16);
+        float hexOneGVal = Integer.parseInt(hexOne.substring(2, 4),16);
+        float hexOneBVal = Integer.parseInt(hexOne.substring(4, 6),16);
+
+        float hexTwoRVal = Integer.parseInt(hexTwo.substring(0, 2),16);
+        float hexTwoGVal = Integer.parseInt(hexTwo.substring(2, 4),16);
+        float hexTwoBVal = Integer.parseInt(hexTwo.substring(4, 6),16);
+
+        //Adjusts the length so that steps are calculated with no spaces
+        int adjLength = 0;
+        for(int i = 0; i < input.length(); i++){
+            if(!input.substring(i, i+1).equals(" ")) adjLength++;
+        }
+
+        //Calculates the amount of steps to take
+        float steps = (adjLength - 1);
+
+        //Vars for holding result colors
+        float resRed;
+        float resGreen;
+        float resBlue;
+
+        //Loop through each step
+        for(float j = 0; j <= steps; j++){
+
+            //Calculate what percentage of the color should be faded
+            float percent = (j / steps);
+
+            //Add that fade to the result color
+            resRed = hexOneRVal + Math.round(percent * (hexTwoRVal - hexOneRVal));
+            resGreen = hexOneGVal + Math.round(percent * (hexTwoGVal - hexOneGVal));
+            resBlue = hexOneBVal + Math.round(percent * (hexTwoBVal - hexOneBVal));
+
+            //Add leading zeros to all gex values
+            StringBuilder resRHex = new StringBuilder(Integer.toHexString((int)resRed));
+            while(resRHex.length() < 2) resRHex.insert(0, "0");
+
+            StringBuilder resBHex = new StringBuilder(Integer.toHexString((int)resBlue));
+            while(resBHex.length() < 2) resBHex.insert(0, "0");
+
+            StringBuilder resGHex = new StringBuilder(Integer.toHexString((int)resGreen));
+            while(resGHex.length() < 2) resGHex.insert(0, "0");
+
+            StringBuilder hexToAdd = new StringBuilder(resRHex.toString() + resGHex.toString() + resBHex.toString());
+            while(hexToAdd.length() < 6) hexToAdd.insert(0, "0"); //This should never be reached...
+
+            //If the character isn't a space, append the code and the value
+            if(input.charAt((int)j) != ' '){
+                output.append("&#" + hexToAdd.toString().toUpperCase() + Character.toString(input.charAt((int)j)));
+            }
+            //Else, append a space
+            else output.append(" ");
+        }
+
+        return output.toString();
+    }
+
+    public static String blendMain(int howManyCodes, String input, String[] codeArray){
+
+        //New builder
+        StringBuilder output = new StringBuilder();
+
+        //This is easy - Blend the colors, return it
+        if(howManyCodes == 2) return(blendTwo(codeArray[0], codeArray[1], input));
+        //This is not - Figure out if it's odd or even, blend from there, recursively
+        else blendHard(howManyCodes, input, codeArray, output);
+
+        return output.toString();
+    }
+
+    public static void blendHard(int howManyCodes, String input, String[] codeArray, StringBuilder output){
+
+        //Create a new array to hold separated pieces
+        String[] inputSeparated = new String[5];
+
+        //If the midpoint number is even
+        if((howManyCodes - 2) % 2 == 0) blendEven(howManyCodes - 2, 0, (input.length() - 1), inputSeparated, input);
+        //If midpoint number is odd
+        else blendOdd(howManyCodes - 2, 0, (input.length() - 1), inputSeparated, input);
+
+        //Goes through and concatenates blended pieces together into one output
+        for (int i = 0; i <= 4; i++) {
+            if (inputSeparated[i] != null && !inputSeparated[i].equals("")) {
+                if (i == 0) {
+                    output.append(blendTwo(codeArray[i], codeArray[i + 1], inputSeparated[i]));
+                }
+                else {
+                    String temp = blendTwo(codeArray[i], codeArray[i + 1], inputSeparated[i]);
+                    output.append(temp.substring(9, temp.length()));
+                }
+            }
+        }
+    }
+
+    public static void blendEven(int midpoints, int start, int end, String[] inputSeparated, String input){
+
+        int midpoint1;
+        int midpoint2;
+        int midpoint3;
+        int midpoint4;
+
+        if(midpoints == 2){
+            midpoint1 = start + 2;
+            midpoint2 = end - 2;
+
+            while(((midpoint2 - midpoint1) - midpoint1) >= 2) {
+                midpoint1 += 1;
+                midpoint2 -= 1;
+            }
+
+            inputSeparated[0] = input.substring(start, midpoint1 + 1);
+            inputSeparated[1] = input.substring(midpoint1, midpoint2 + 1);
+            inputSeparated[2] = input.substring(midpoint2, end + 1);
+        }
+        else if(midpoints == 4){
+            midpoint1 = start + 2;
+            midpoint2 = midpoint1 + 2;
+            midpoint4 = end - 2;
+            midpoint3 = midpoint4 - 2;
+
+            while(Math.abs((midpoint2 - midpoint1) - (midpoint3 - midpoint2)) >= 4){
+                midpoint1 += 1;
+                midpoint2 += 2;
+                midpoint4 -= 1;
+                midpoint3 -= 2;
+            }
+
+            inputSeparated[0] = input.substring(start, midpoint1 + 1);
+            inputSeparated[1] = input.substring(midpoint1, midpoint2 + 1);
+            inputSeparated[2] = input.substring(midpoint2, midpoint3 + 1);
+            inputSeparated[3] = input.substring(midpoint3, midpoint4 + 1);
+            inputSeparated[4] = input.substring(midpoint4, end + 1);
+        }
+    }
+
+    public static void blendOdd(int midpoints, int start, int end, String[] inputSeparated, String input){
+        int midpoint1;
+        int midpoint2;
+        int midpoint3;
+
+        if(midpoints == 1){
+            if (input.length() % 2 == 1) {
+                midpoint1 = end / 2;
+            }
+            else {
+                midpoint1 = (end + 1) / 2;
+            }
+
+            inputSeparated[0] = input.substring(start, midpoint1 + 1);
+            inputSeparated[1] = input.substring(midpoint1, end + 1);
+        }
+        else if(midpoints == 3){
+            //Calculate center midpoint (midpoint2)
+            if (input.length() % 2 == 1) {
+                midpoint2 = end / 2;
+            }
+            else {
+                midpoint2 = (end + 1) / 2;
+            }
+
+            //Calculate midpoint1
+            if (midpoint2 % 2 == 0) {
+                midpoint1 = (midpoint2 / 2);
+            }
+            else {
+                midpoint1 = ((midpoint2 + 1) / 2);
+            }
+
+            //Calculate midpoint3
+            if ((end - midpoint2) % 2 == 1) {
+                midpoint3 = (end - (midpoint2 / 2));
+            }
+            else {
+                midpoint3 = (end - ((midpoint2 + 1) / 2));
+            }
+
+            inputSeparated[0] = input.substring(start, midpoint1 + 1);
+            inputSeparated[1] = input.substring(midpoint1, midpoint2 + 1);
+            inputSeparated[2] = input.substring(midpoint2, midpoint3 + 1);
+            inputSeparated[3] = input.substring(midpoint3, end + 1);
+        }
+    }
+
+    public static boolean isNumeric(String str) { 
+        try {  
+          Integer.parseInt(str);  
+          return true;
+        } 
+        catch(NumberFormatException e){  
+          return false;  
+        }  
+    }
+
+    public static boolean isHexOk(String hex){
+        if(hex.length() != 6) return false;
+        //Starts a counter for how many valid chars there are
+        int okCount = 0;
+        //For each character in hex     //For each valid hex char               //If it's a valid hex character, increment
+        for(int i = 0; i <= 5; i++) for(int j = 0; j <= 15; j++) if(hex.toUpperCase().charAt(i) == ("0123456789ABCDEF".charAt(j))) okCount++;
+        //If it's 6, good, else, bad
+        return okCount == 6;
+    }
+
+}
---
+++ src/main/java/com/BlendGUI.java
@@ -0,0 +1,480 @@
+package com;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+//Clipboard management
+import java.awt.datatransfer.StringSelection;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+//Logging
+import java.util.logging.*;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+
+public class BlendGUI extends Application {
+
+    Logger log = Logger.getLogger(BlendGUI.class.getSimpleName());
+
+    String defaultFont = "Arial";
+
+    protected static String currentNick;
+    protected LimitedTextField lastField;
+
+    protected static double defaultPreviewHeight;
+
+    public boolean tryLoad(Stage stage, LimitedTextField[] codeFields){
+
+        String[] codes = new String[6];
+
+        FileChooser configChooser = new FileChooser();
+        configChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("BHB Configs (.txt)", "*.txt"));
+        configChooser.setTitle("Open configuration file");
+        File configFile = configChooser.showOpenDialog(stage);
+        if(configFile != null){
+            try( FileReader fReader = new FileReader(configFile); //Basic reader, throws FileNotFoundException
+            BufferedReader bReader = new BufferedReader(fReader);)
+            { 
+                //Read the codes
+                for(int i = 0; i < 6; i++) {
+                    String line = bReader.readLine();
+                    if(Blend.isHexOk(line)){
+                        codes[i] = line;
+                    }
+                    else return false;
+                }
+                for(int i = 0; i < 6; i++) codeFields[i].setText(codes[i]);
+                return true;
+            }
+            catch(IOException | StringIndexOutOfBoundsException e)
+            {
+                log.log(Level.SEVERE, e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    public void trySave(Stage stage, LimitedTextField[] codeFields){
+
+        int goodCodes = 0;
+
+        for(int i = 0; i < 6; i++) if(Blend.isHexOk(codeFields[i].getText())) goodCodes++;
+
+        if(goodCodes >= 1){
+            FileChooser configChooser = new FileChooser();
+            configChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("BHB Configs (.txt)", "*.txt"));
+            configChooser.setTitle("Save configuration file");
+            File saveFile = configChooser.showSaveDialog(stage);
+
+            if(saveFile != null){
+
+                String[] codes = new String[6];
+                for(int i = 0; i < 6; i++){
+                    if(Blend.isHexOk(codeFields[i].getText())) codes[i] = codeFields[i].getText();
+                }
+
+                try( FileWriter fWriter = new FileWriter(saveFile); //Basic reader, throws FileNotFoundException
+                BufferedWriter bWriter = new BufferedWriter(fWriter);)
+                { 
+                    for(int i = 0; i < 6; i++){
+                        bWriter.write(codes[i]);
+                        if(i != 5){
+                            bWriter.newLine();
+                        }
+                    }
+                }
+                catch(IOException | StringIndexOutOfBoundsException e)
+                {
+
+                    log.log(Level.SEVERE, e.getMessage());
+
+                    Alert errorAlert = new Alert(AlertType.ERROR);
+                    errorAlert.setHeaderText("Error saving");
+                    errorAlert.setContentText("There was an error saving your configuration, please try again.");
+                    errorAlert.showAndWait();
+                }
+            }
+        }
+        else{
+            Alert errorAlert = new Alert(AlertType.ERROR);
+            errorAlert.setHeaderText("Nothing to save");
+            errorAlert.setContentText("You do not have any codes to save. No file was created.");
+            errorAlert.showAndWait();
+        }  
+       
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        LimitedTextField[] codeFields = new LimitedTextField[6];
+
+        MenuBar menuBar = new MenuBar();
+        // Menu - File
+        Menu menuFile = new Menu("File");
+
+        MenuItem saveItem = new MenuItem("Save");
+        saveItem.setOnAction( e -> {
+            trySave(stage, codeFields);
+        });
+
+        MenuItem loadItem = new MenuItem("Load");
+        loadItem.setOnAction( e -> {
+            boolean success = tryLoad(stage, codeFields);
+            if(!success){
+                Alert errorAlert = new Alert(AlertType.ERROR);
+                errorAlert.setHeaderText("Invalid configuration file");
+                errorAlert.setContentText("The file you selected is not recognized as a valid configuration file. If you believe this is an error, please reach out.");
+                errorAlert.showAndWait();
+            }
+        });
+
+        menuFile.getItems().addAll(saveItem, loadItem);
+
+
+        // Menu - Edit
+        Menu menuEdit = new Menu("Tools");
+
+        MenuItem copyItem = new MenuItem("Copy To Clipboard");
+        copyItem.setOnAction( e -> {
+            if(currentNick != null && !currentNick.equals("")){
+                StringSelection stringSelection = new StringSelection(currentNick);
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboard.setContents(stringSelection, null);
+            }
+        });
+
+        menuEdit.getItems().add(copyItem);
+
+        menuBar.getMenus().addAll(menuFile, menuEdit);
+
+        VBox mainBox = new VBox();
+
+        HBox previewLabels = new HBox();
+        previewLabels.setAlignment(Pos.CENTER);
+
+        BorderPane previewCopyPane = new BorderPane();
+        previewCopyPane.setMinHeight(50);
+        previewCopyPane.setCenter(previewLabels);
+        previewCopyPane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.THIN)));
+        previewCopyPane.setPadding(new Insets(5));
+
+        LimitedTextField enterNickName = new LimitedTextField();
+        enterNickName.setRestrict("[A-Za-z0-9_]");
+        enterNickName.setPrefWidth(275);
+        enterNickName.textProperty().addListener((observable, oldValue, newValue) -> updatePreview(enterNickName, previewLabels, codeFields));
+        
+        //Holds the color code selector and the hex color picker
+        HBox codesAndPicker = new HBox();
+
+        HBox code1 = makeCodeBox(1, codeFields, enterNickName, previewLabels);
+        HBox code2 = makeCodeBox(2, codeFields, enterNickName, previewLabels);
+        HBox code3 = makeCodeBox(3, codeFields, enterNickName, previewLabels);
+        HBox code4 = makeCodeBox(4, codeFields, enterNickName, previewLabels);
+        HBox code5 = makeCodeBox(5, codeFields, enterNickName, previewLabels);
+        HBox code6 = makeCodeBox(6, codeFields, enterNickName, previewLabels);
+
+        unlockFields(codeFields);
+
+        Button clearAllCodes = new Button("Clear All");
+
+        VBox codes = new VBox(code1, code2, code3, code4, code5, code6, clearAllCodes);
+        codes.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.THIN)));
+        codes.setPadding(new Insets(5));
+        codes.setAlignment(Pos.CENTER);
+        codes.setSpacing(3);
+
+        //COLORPICKER CODE WOOOOOOO
+        VBox colorPicker = new VBox();
+        colorPicker.setAlignment(Pos.CENTER);
+        colorPicker.setMinHeight(160);
+        colorPicker.setMinWidth(160);
+        colorPicker.setPrefWidth(220);
+        colorPicker.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.THIN)));
+
+        Circle colorCircle = new Circle();
+        colorCircle.setRadius(75);
+
+        ColorPicker colorPickerUI = new ColorPicker(Color.BLACK);
+        colorPickerUI.setOnAction(e ->{
+            colorCircle.setFill(colorPickerUI.getValue());
+        });
+
+        clearAllCodes.setOnAction(e -> {
+            for(LimitedTextField f : codeFields) f.clear();
+            previewLabels.getChildren().clear();
+            enterNickName.clear();
+            colorPickerUI.setValue(Color.BLACK);
+            colorCircle.setFill(Color.BLACK);
+            code1.requestFocus();
+        });
+
+        colorCircle.setFill(colorPickerUI.getValue());
+
+        HBox pickerAndCopyButton = new HBox();
+        pickerAndCopyButton.setPadding(new Insets(5));
+        pickerAndCopyButton.setAlignment(Pos.CENTER);
+        pickerAndCopyButton.setSpacing(10);
+
+        Button copyToFirstEmpty = new Button("<<<");
+        copyToFirstEmpty.setFont(Font.font(defaultFont, FontWeight.SEMI_BOLD, 14));
+        copyToFirstEmpty.setOnAction( e -> {
+
+            int findMe = 0;
+
+            for(int i = 0; i < 6; i++){
+                if (codeFields[i].equals(lastField)){
+                    findMe = i;
+                    break;
+                }
+            }
+
+            if(lastField != null && !lastField.isDisabled()){
+                lastField.setText(colorPickerUI.getValue().toString().substring(2,8).toUpperCase());
+                if(findMe + 1 < 6){
+                    lastField = codeFields[findMe + 1];
+                }
+            }
+            else{
+                for(int i = 0; i < 6; i++){
+                    if(!Blend.isHexOk(codeFields[i].getText())){
+                        codeFields[i].setText(colorPickerUI.getValue().toString().substring(2,8).toUpperCase());
+                        break;
+                    }
+                }
+            } 
+            
+            lastField.requestFocus();
+
+        });
+
+        pickerAndCopyButton.getChildren().addAll(copyToFirstEmpty, colorPickerUI);
+
+        colorPicker.getChildren().addAll(colorCircle, pickerAndCopyButton);
+        colorPicker.setSpacing(10);
+
+        codesAndPicker.getChildren().addAll(codes, colorPicker);
+        codesAndPicker.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.THIN)));
+
+        HBox nickInput = new HBox(new Label("Enter nickname: "), enterNickName);
+        nickInput.setAlignment(Pos.CENTER);
+        nickInput.setPadding(new Insets(5));
+        nickInput.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.THIN)));
+
+        mainBox.getChildren().addAll(codesAndPicker, nickInput, previewCopyPane);
+
+        VBox emptyBox = new VBox();
+        emptyBox.setMaxSize(0, 0);
+
+        Scene mainScene = new Scene(mainBox);
+        ((VBox)mainScene.getRoot()).getChildren().addAll(menuBar);
+
+        menuBar.toBack();
+        mainBox.toFront();
+
+        stage.setScene(mainScene);
+        stage.setTitle("Blazin's Hex Blender");
+        stage.show();
+        stage.setResizable(false);
+
+    }
+
+    public Label makePreviewLabel(){ 
+        Label newLabel = new Label("     ");
+        newLabel.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.THIN)));
+        newLabel.setPadding(new Insets(3));
+        return newLabel;
+    }
+
+    public HBox makeCodeBox(int id, LimitedTextField[] codeFields, TextField userInField, HBox previewLabels){
+        Label codeId = new Label("Code " + id + ": ");
+        Label codeColorPreview = makePreviewLabel();
+        LimitedTextField codeField = makeTextField(codeColorPreview, codeFields, id, userInField, previewLabels);
+        codeField.setRestrict("[a-fA-F0-9]");
+
+        codeId.setOnMouseClicked(e -> codeField.setText(generateRandomHex()));
+
+        codeColorPreview.setOnMouseClicked(e -> codeField.clear());
+
+        HBox newBox = new HBox(codeId, codeField, codeColorPreview);
+        newBox.setAlignment(Pos.CENTER);
+        newBox.setSpacing(6);
+
+        return newBox;
+    }
+
+    public LimitedTextField makeTextField(Label previewLabel, LimitedTextField[] codeFields, int id, TextField userInField, HBox previewLabels){
+        LimitedTextField newField = new LimitedTextField();      
+
+        newField.setPrefWidth(75);
+        newField.setFont(new Font(defaultFont, 14));
+
+        newField.setMaxLength(6);
+
+        newField.pressedProperty().addListener(listener -> lastField = newField);
+
+        newField.focusedProperty().addListener(listener -> lastField = newField);
+
+        newField.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            if(Blend.isHexOk(newValue)){
+                previewLabel.setBackground(new Background(new BackgroundFill(Color.rgb(
+                    Integer.parseInt(newValue.substring(0,2),16),
+                    Integer.parseInt(newValue.substring(2,4),16),
+                    Integer.parseInt(newValue.substring(4,6),16)), CornerRadii.EMPTY, Insets.EMPTY)));
+            }
+            else{
+                previewLabel.setBackground(new Background(new BackgroundFill(Color.rgb(
+                    Integer.parseInt("F2",16),
+                    Integer.parseInt("F2",16),
+                    Integer.parseInt("F2",16)), CornerRadii.EMPTY, Insets.EMPTY)));
+            }
+            unlockFields(codeFields);
+            updatePreview(userInField, previewLabels, codeFields);
+            updateTextFieldFontSize(newField);
+        });
+
+        codeFields[id - 1] = newField;
+
+        return newField;
+    }
+
+    public void unlockFields(LimitedTextField[] codefields){
+
+        for(int i = 0; i < 6; i++){
+
+            if(codefields[i].getText().length() == 6 && !codefields[i].isDisable() && i + 1 < 6 && Blend.isHexOk(codefields[i].getText())){
+                codefields[i + 1].setDisable(false);
+            }
+            else if (i + 1 < 6) {
+                codefields[i + 1].setDisable(true);      
+            }
+        }
+
+    }
+
+    public static void updateTextFieldFontSize(LimitedTextField field){
+        if(field.getText().length() > 0){
+            int fontSize = 85 / field.getText().length();
+            if (fontSize > 12) fontSize = 14;
+            field.setFont(new Font("Arial", fontSize));
+        }    
+    }
+
+    public static String updatePreview(TextField userIn, HBox previewLabels, LimitedTextField[] codeFields){
+
+        int userInputLength = userIn.getText().length();
+
+        //Nick needs to be at least 3 chars
+        if(userInputLength >= 3){
+
+            //Need to be at least 2 codes
+            int validCodes = 0;
+            for(int i = 0; i <= 5; i++) if(!codeFields[i].isDisabled() && Blend.isHexOk(codeFields[i].getText())) validCodes++;
+            if (validCodes >= 2){
+
+                //Need to have certain number of characters for more codes
+                boolean minCharsMet = false;
+                if(userInputLength >= ((validCodes * 2) - 1)) minCharsMet = true;
+                
+                if(minCharsMet){
+
+                    String[] codeArray = new String[validCodes];
+                    for(int i = 0; i < validCodes; i++){
+                        codeArray[i] = codeFields[i].getText();
+                    }
+
+                    previewLabels.setPrefHeight(defaultPreviewHeight);
+                    currentNick = Blend.blendMain(validCodes, userIn.getText(), codeArray);
+                    parseNickToLabel(currentNick, previewLabels);
+                }
+                else{
+                    previewLabels.getChildren().clear();
+                    previewLabels.setPrefHeight(0);
+                    currentNick = "";
+                }
+            }
+            else{
+                previewLabels.getChildren().clear();
+                previewLabels.setPrefHeight(0);;
+                currentNick = "";
+            }
+        }
+        else{
+            previewLabels.getChildren().clear();
+            previewLabels.setPrefHeight(0);
+            currentNick = "";
+        }
+        return null;
+    }
+
+    public static void parseNickToLabel(String nick, HBox previewLabels){
+
+        previewLabels.getChildren().clear();
+
+        String[] comp = nick.split("&#");
+
+        for(int i = 1; i < comp.length; i++){
+            previewLabels.getChildren().add(makePreviewLabel(comp[i].charAt(6), comp[i].substring(0,6),  (comp.length - 1)));
+        }
+
+    }
+
+    public static Label makePreviewLabel(char c, String hex, int fullLength){
+        Label previewLabel = new Label(Character.toString(c));
+        previewLabel.setTextFill(Color.rgb(Integer.parseInt(hex.substring(0,2), 16), Integer.parseInt(hex.substring(2,4), 16), Integer.parseInt(hex.substring(4,6), 16)));
+
+        double fontSize = Math.floor(650.0 / fullLength);
+        if(fontSize > 30) fontSize = 30;
+
+        previewLabel.setFont(new Font("Arial", fontSize));
+        return previewLabel;
+    }
+
+    public String generateRandomHex(){
+
+        Random random = new Random();
+        String rndHex = "";
+
+        for (int i = 0; i < 6; i++) rndHex += "ABCDEF0123456789".charAt(random.nextInt(16));
+
+        return rndHex;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+    
+}
